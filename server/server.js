a44b11abedf9184fe3bed493e50c9a38fbf7216e
soondong.park <soondong.park@lge.com>
2015-05-19 10:37:53 +0900

    EJS 적용 page -> view, part -> partial-view로 디렉토리 이름 변경

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var process = require('process');
 var fs = require('fs');
+var path = require('path');
 var PORT = process.env['port'] || "";
 
 // 웹서버 객체
@@ -13,31 +14,35 @@ var WebServer = {
     Init : function() {
         var _this = this;
 
+        app.set('view engine', 'ejs');
+        app.set('views', path.join(__dirname, '/../client/template'));
+
         // mongodb 초기화
         /*MongoClient.connect("mongodb://localhost:27027/hooki", function(err, db) {
-            if (err)
-                throw err;
-            else
-                _this.mongoDb = db;
+        if (err)
+        throw err;
+        else
+        _this.mongoDb = db;
         });*/
 
         // body parser middleware 사용
         app.use(bodyParser.json());
 
         // static 파일 요청 처리
-        app.use('/static', express.static(__dirname + '/../client/static'));
+        app.use('/static', express.static(path.join(__dirname, '/../client/static/')));
 
         // page 요청 라우팅
         app.get('/', function(req, res) {
-           res.redirect('/start');
-           res.end();
+            res.redirect('/start');
+            res.end();
         });
 
         // page 요청 라우팅
         app.get('/start', function(req, res) {
-           res.end('start page');
+            res.render('view/start', {
+                title : 'Start page'
+            });
         });
-
 
         app.get('/start/:subPageName', function(req, res) {
             var subPageName = req.params.subPageName;
@@ -45,22 +50,33 @@ var WebServer = {
         });
 
         app.get('/write', function(req, res) {
-            res.end('write page');
+            res.render('view/write', {
+                title : 'Write page'
+            });
         });
 
         app.get('/view/:pageId', function(req, res) {
             var pageId = req.params.pageId;
-            res.end('view page - ' + pageId);
+            res.render('view/view', {
+                title : 'View page',
+                pageId : pageId
+            });
         });
 
         app.get('/profile/:userId', function(req, res) {
             var userId = req.params.userId;
-            res.end('profile page - ' + userId);
+            res.render('view/profile', {
+                title : 'Profile page',
+                userId : userId
+            });
         });
 
-        app.get('/product/:productCode', function(req, res) {
-            var productCode = req.params.productCode;
-            res.end('product page - ' + productCode);
+        app.get('/product/:productId', function(req, res) {
+            var productId = req.params.productId;
+            res.render('view/product', {
+                title : 'Proudct page',
+                productId : productId
+            });
         });
 
         this.server = app.listen(PORT);
